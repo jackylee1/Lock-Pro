0e017376ebc26feeedd9afd27e904e6b473f9146
Navaneet Sarma <navaneet.sarma@diona.com>
2016-03-20 03:53:48 +0530

    images

--- Lock Pro/TabBar.m
+++ Lock Pro/TabBar.m
@@ -17,15 +17,27 @@
         UIViewController *vc1 = [[LocksTab alloc]init];//[[UIViewController alloc]init];
         vc1.view.backgroundColor = [UIColor colorWithRed:0.945 green:0.945 blue:0.945 alpha:1.00];
         vc1.title = @"Doors";
-        //vc1.tabBarItem.image = ;
+        //vc1.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemMostViewed tag:0];
+        vc1.tabBarItem.image = [UIImage imageNamed:@"door"];
+        
         UIViewController *vc2 = [[UIViewController alloc]init];
         vc2.view.backgroundColor = [UIColor lightGrayColor];
         vc2.title = @"Users";
+        //vc2.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemContacts tag:0];
+        vc2.tabBarItem.image = [UIImage imageNamed:@"users"];
+
         UIViewController *vc3 = [[UIViewController alloc]init];
         vc3.view.backgroundColor = [UIColor lightGrayColor];
         vc3.title = @"History";
+        //vc3.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemHistory tag:0];
+        vc3.tabBarItem.image = [UIImage imageNamed:@"history"];
+        
+        UIViewController *vc4 = [[UIViewController alloc]init];
+        vc4.view.backgroundColor = [UIColor lightGrayColor];
+        vc4.title = @"Settings";
+        vc4.tabBarItem.image = [UIImage imageNamed:@"settings"];
 
-        NSArray *Controllers = [[NSArray alloc] initWithObjects:vc1,vc2,vc3, nil];
+        NSArray *Controllers = [[NSArray alloc] initWithObjects:vc1,vc2,vc3,vc4, nil];
         [self setViewControllers:Controllers];
     }
     
